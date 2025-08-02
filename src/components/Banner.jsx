00f8674b736a8b73eb24892b447920311a3f2b87
diyornv <diyorcode@gmail.com>
2025-08-02 19:55:19 +0500

    tailwind config and banner end 3.1 end

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,21 +2,17 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="w-full bg-black relative container">
-      <div className="flex items-center justify-center px-4 py-3">
-        <div className="flex-1 flex justify-center">
-          <span className="text-[#FAFAFA] text-center text-sm leading-[21px]">
-            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!{" "}
-            <span className="font-bold underline cursor-pointer">ShopNow</span>
-          </span>
-        </div>
-        <div className="flex items-center">
-          <select className="bg-black text-white border-none text-sm focus:outline-none">
-            <option>English</option>
-            <option>Русский</option>
-            <option>O'zbekcha</option>
-          </select>
-        </div>
+    <div className="h-[48px] w-full bg-black flex items-center ">
+      <span className="text-[#FAFAFA] text-center text-sm leading-[21px] w-full">
+        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!{" "}
+        <span className="font-bold underline cursor-pointer">ShopNow</span>
+      </span>
+      <div className="ml-auto flex items-center" style={{ marginRight: "200px" }}>
+        <select className="bg-black text-white border-none text-sm focus:outline-none">
+          <option>English</option>
+          <option>Русский</option>
+          <option>O'zbekcha</option>
+        </select>
       </div>
     </div>
   );
