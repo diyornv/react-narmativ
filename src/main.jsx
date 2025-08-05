404626b7ad142c2587b865db319f92dc68f9ef2c
diyornv <diyorcode@gmail.com>
2025-08-05 12:42:10 +0500

    3.5 end

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Banner />
     <Navbar />
+    <div className="container flex">
+      <Sidebar />
+    </div>
   </React.StrictMode>
 );
