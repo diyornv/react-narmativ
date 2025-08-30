16b3c073c0fead04e770ad5ae0934619e9cfb9fc
diyornv <diyorcode@gmail.com>
2025-08-30 13:24:50 +0500

    3.21

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,21 +5,17 @@ import googleIcon from "../assets/svg/Icon-Google.svg";
 const SignIn = () => {
   return (
     <div className="py-20 flex">
-      <div className="">
-        <img
-          src={signIn}
-          alt="Phone and Shopping Cart"
-          className=" object-cover"
-        />
+      <div>
+        <img src={signIn} alt="signin img" className="object-cover" />
       </div>
 
       <div className="w-1/2 flex items-center justify-center">
         <div className="w-full max-w-md px-8">
-          <div className=" mb-8">
-            <h1 className="text-4xl font-semibold text-gray-900 mb-2">
+          <div className="mb-8">
+            <h1 className="text-4xl font-medium text-black">
               Create an account
             </h1>
-            <p className="text-gray-600">Enter your details below</p>
+            <p>Enter your details below</p>
           </div>
 
           <form className="space-y-6">
@@ -27,49 +23,42 @@ const SignIn = () => {
               <input
                 type="text"
                 placeholder="Name"
-                className="w-full py-3 border-b border-gray-300 focus:outline-none focus:border-gray-900"
+                className="w-full py-3 border-b border-[#999999] focus:outline-none focus:border-black"
               />
             </div>
-
             <div>
               <input
                 type="email"
                 placeholder="Email or Phone Number"
-                className="w-full py-3 border-b border-gray-300 focus:outline-none focus:border-gray-900"
+                className="w-full py-3 border-b border-[#999999] focus:outline-none focus:border-black"
               />
             </div>
-
             <div>
               <input
                 type="password"
                 placeholder="Password"
-                className="w-full py-3 border-b border-gray-300  focus:outline-none focus:border-gray-900"
+                className="w-full py-3 border-b border-[#999999] focus:outline-none focus:border-black"
               />
             </div>
 
             <button
               type="submit"
-              className="w-full bg-[#DB4444] text-white py-3 rounded-sm font-medium cursor-pointer"
+              className="w-full bg-[#DB4444] text-white py-3 rounded-sm cursor-pointer"
             >
               Create Account
             </button>
           </form>
-
           <div className="mt-6">
-            <button className="w-full flex items-center justify-center gap-3 py-3 border border-gray-300 rounded-sm cursor-pointer">
-              <img src={googleIcon} alt="Google" className="w-5 h-5" />
+            <button className="w-full flex items-center justify-center gap-3 py-3 border border-[#999999] rounded-sm cursor-pointer">
+              <img src={googleIcon} alt="google" className="w-5 h-5" />
               <span className="text-gray-700">Sign up with Google</span>
             </button>
           </div>
-
           <div className="mt-6 text-center">
-            <p className="text-gray-600">
+            <p className="text-[#4D4D4D]">
               Already have account?
-              <a
-                href="#"
-                className="text-[#DB4444] font-medium hover:underline"
-              >
-                Log in
+              <a href="#" className="text-[#4D4D4D] font-medium underline">
+                Login
               </a>
             </p>
           </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -5,49 +5,43 @@ import googleIcon from "../assets/svg/Icon-Google.svg";
 const SignUp = () => {
   return (
     <div className="py-20 flex">
-      <div className="">
-        <img
-          src={signIn}
-          alt="Phone and Shopping Cart"
-          className=" object-cover"
-        />
+      <div>
+        <img src={signIn} alt="signin img" className="object-cover" />
       </div>
 
       <div className="w-1/2 flex items-center justify-center">
         <div className="w-full max-w-md px-8">
-          <div className=" mb-8">
-            <h1 className="text-4xl font-semibold text-gray-900 mb-2">
+          <div className="mb-8">
+            <h1 className="text-4xl font-medium text-black">
               Log in to Exclusive
             </h1>
-            <p className="text-gray-600">Enter your details below</p>
+            <p>Enter your details below</p>
           </div>
 
           <form className="space-y-6">
             <div>
               <input
-                type="text"
+                type="email"
                 placeholder="Email or Phone Number"
-                className="w-full py-3 border-b border-gray-300 focus:outline-none focus:border-gray-900"
+                className="w-full py-3 border-b border-[#999999] focus:outline-none focus:border-black"
               />
             </div>
-
             <div>
               <input
                 type="password"
                 placeholder="Password"
-                className="w-full py-3 border-b border-gray-300  focus:outline-none focus:border-gray-900"
+                className="w-full py-3 border-b border-[#999999] focus:outline-none focus:border-black"
               />
             </div>
           </form>
-
-          <div className="mt-6 flex items-center justify-between">
+          <div className="mt-6 flex justify-between items-center">
             <button
-              type="Login"
-              className="bg-[#DB4444] px-12 text-white py-3 rounded-sm font-medium outline-none cursor-pointer"
+              type="submit"
+              className="bg-[#DB4444] text-white px-12 py-4 rounded-sm cursor-pointer"
             >
               Login
             </button>
-            <a href="#" className="text-[#DB4444] font-medium hover:underline">
+            <a href="#" className="text-[#DB4444] font-medium underline">
               Forget Password?
             </a>
           </div>
